461db0754abb9717f0fe0b95328f5bb8da3f653d
keshavkumar4699 <kumar.keshav4699@gmail.com>
2024-07-03 23:45:43 +0530

    successfully checks if torrent is freeleech

--- test.py
+++ test.py
@@ -1,16 +1,8 @@
-### using selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
-# from selenium import webdriver
-# browser = webdriver.Firefox()
-# browser.get('https://www.google.com')
-
-### using basic 
-
-# import webbrowser
-# url = "https://www.myanonamouse.net/t/1050434"
-# webbrowser.open(url)
-
-### fetching html file contents from html file
+# fetching html file contents from html file
 
 # from bs4 import BeautifulSoup
 # with open('test.html', 'r', encoding='utf-8') as file:
@@ -19,27 +11,41 @@
 # text = soup.get_text()
 # print(soup)
 
-### fetching html file contents from webpage
+# fetching html file contents from webpage
+def get_torrent(url):
+    driver = webdriver.Firefox()
+    # Open the webpage
+    driver.get(url)
+    # Enter login credentitals
+    username_field = driver.find_element(By.NAME, "email")
+    password_field = driver.find_element(By.NAME, "password")
+    username_field.send_keys("sweatik2@gmail.com")
+    password_field.send_keys("K1a12i@7106")
+    # Submit the form
+    submit_button = driver.find_element(By.CSS_SELECTOR, "input[value='Log in!']")
+    submit_button.click()
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+    # Get the HTML content
+    html_content = driver.page_source
+    # Parse the HTML using BeautifulSoup
+    soup = BeautifulSoup(html_content, "html.parser")
+    # Find the <div> with ID "ratio"
+    div_ratio = soup.find("div", id="ratio")
+    span = div_ratio.find_all("div")[1].find("span")
 
-# Set the path to your geckodriver executable
-# geckodriver_path = '/path/to/geckodriver'
-# Create a Firefox webdriver
-driver = webdriver.Firefox()
-# Open the webpage
-driver.get("https://www.myanonamouse.net/t/1050434")
-# Get the HTML content
-html_content = driver.page_source
-print(html_content)
-# Enter login credentitals
-username_field = driver.find_element(By.NAME, "email")
-password_field = driver.find_element(By.NAME, "password")
-username_field.send_keys("sweatik2@gmail.com")
-password_field.send_keys("K1a12i@7106")
-# Submit the form
-submit_button = driver.find_element(By.CSS_SELECTOR, "input[value='Log in!']")
-submit_button.click()
-# Close the browser
-# driver.quit()
+    # Close the browser
+    driver.quit()
+    return span.text
+
+# function to check ratio text
+def check_freeleech_flag(input_str):
+    lower_str = input_str.lower()
+    # Check if freeleech word is present or not
+    if "freeleech" in lower_str:
+        return True
+    else: 
+        return  False
+
+url = "https://www.myanonamouse.net/t/1050720"
+span = get_torrent(url)
+print(check_freeleech_flag(span))
