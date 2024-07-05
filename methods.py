7fa3cdf0c994044001a4e16e15823d0ab76f659e
keshavkumar4699 <kumar.keshav4699@gmail.com>
2024-07-05 05:49:36 +0530

    downloading torrents for objective 1 done

--- methods.py
+++ methods.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
+# function to login into myanonamouse.net
 
 def mam_login(driver):
     driver.get("https://www.myanonamouse.net/index.php")
@@ -14,10 +15,8 @@ def mam_login(driver):
     submit_button = driver.find_element(
         By.CSS_SELECTOR, "input[value='Log in!']")
     submit_button.click()
-    return
 
-# this method return soup object after parsing the site fetched from url
-
+# function to return soup object after parsing the site fetched from url
 
 def get_site(driver, url):
     # Open the webpage
@@ -28,8 +27,7 @@ def get_site(driver, url):
     soup = BeautifulSoup(html_content, "html.parser")
     return soup
 
-# check if torrent is freeleech or not
-
+# function to check if torrent is freeleech or not
 
 def is_freeleech(soup):
     # Find the <div> with ID "ratio"
@@ -43,10 +41,13 @@ def is_freeleech(soup):
     else:
         return False
 
+# function to parse size into bytes for easy comparison
 
 def parse_size(size_str):
     # Split the size string into value and unit
     value, unit = size_str.split()
+    #remove commas from string value
+    value = value.replace(",", "")
     # Convert value to float
     value = float(value)
     # Map units to bytes (assuming 1 GiB = 1024 MiB = 1024 KiB = 1024 bytes)
@@ -59,6 +60,7 @@ def parse_size(size_str):
     bytes_size = value * unit_to_bytes[unit]
     return bytes_size
 
+# function to check if torrent is of required size or not
 
 def is_required_size(soup, max_size):
     # Find the <div> with ID "size"
@@ -73,30 +75,46 @@ def is_required_size(soup, max_size):
         return False
 
 # funtion to download torrent
+
 def download_torrent(driver):
     # find download button
     download = driver.find_element(By.ID, "tddl")
     download.click()
-    return
 
 # funtion to download the batch of urls
+
 def download_batch(urls, driver):
     for item in urls:
         soup = get_site(driver, item)
         if (is_freeleech(soup) or is_required_size(soup, '2 MiB')):
             download_torrent(driver)
-        else:
-            return
+        
+# function to fetch urls from any snatch table
+
+def fetch_url(file_name):
+    # Read the HTML file
+    with open(file_name, 'r', encoding='utf-8') as file:
+        html_table = file.read()
+    # Parse the HTML content
+    soup = BeautifulSoup(html_table, 'html.parser')
+    rows = soup.find_all('tr')
+    # store all urls in array
+    href_values = []
+    for row in rows:
+        cols = row.find_all('td')
+        if len(cols) >= 3:
+            a_tag = cols[2].find('a')
+            if a_tag:
+                href_value = a_tag['href']
+                href_values.append('https://www.myanonamouse.net' + href_value)
+    return href_values
 
 
 driver = webdriver.Firefox()
 mam_login(driver)
 
-urls = ["https://www.myanonamouse.net/t/516506", "https://www.myanonamouse.net/t/510763",
-        "https://www.myanonamouse.net/t/927929", "https://www.myanonamouse.net/t/1051062", 
-        "https://www.myanonamouse.net/t/1051050","https://www.myanonamouse.net/t/1051055"
-        "https://www.myanonamouse.net/t/1051047","https://www.myanonamouse.net/t/1051023"
-        "https://www.myanonamouse.net/t/1051058","https://www.myanonamouse.net/t/1051057"]
+urls = fetch_url('snatch_table.html')
+print(len(urls))
 
 download_batch(urls, driver)
 
